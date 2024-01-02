bcb7d72cc22c3ca4dba2570ca7bdbfaf09989999
bupd <bupdprasanth@gmail.com>
2024-01-03 05:10:49 +0530

    Created Models in Chatapp.
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- chatapp/models.py
+++ chatapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+class Room(models.Model):
+    name=models.CharField(max_length=20)
+    slug=models.SlugField(max_length=10)
+
+class Message(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    room=models.ForeignKey(Room,on_delete=models.CASCADE)
+    message=models.TextField()
+    timestamp=models.DateTimeField(auto_now_add=True)
